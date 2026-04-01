439014b2186797acb90d7282badefd6b59138477
HamitMizrak <hamitmizrak@gmail.com>
2026-04-01 15:47:18 +0300

    Loop

---
+++ python_pure/_08_loop.py
@@ -0,0 +1,30 @@
+# Loop
+number = [1,2,3,4,5,6,7,8,9]
+
+# For- each Döngüsü
+for temp in number:
+    print(temp)
+print("\n##################")
+
+# For- each Döngüsü
+for temp in range(1,10,2):
+    print(temp)
+
+
+# For- each Döngüsü
+print("\n########")
+for temp in range(1,10,1):
+    print(temp)
+
+print("\n########")
+i=0
+while i<5:
+    print(i)
+    i+=1
+    #i=i+1
+
+# sonsuz döngü
+# while True:
+#     print("Sonsuz Döngü")
+
+
