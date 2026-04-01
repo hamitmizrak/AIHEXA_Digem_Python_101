f2fda386fb8fb52dde813e62f04dc3f1f93a6bad
HamitMizrak <hamitmizrak@gmail.com>
2026-04-01 14:44:03 +0300

    Math

---
+++ python_pure/_05_Math.py
@@ -0,0 +1,21 @@
+# Math
+import math
+from random import randint, uniform
+
+print("PI: ", math.pi)
+print("E: ", math.e)
+
+
+print("Karekök: ", math.sqrt(16))
+print("üslü: ", math.pow(2,5))
+print("faktöriyel: ", math.factorial(4))
+print("yukarı yuvarla: ", math.ceil(4.001))
+print("aşağı yuvarla: ", math.floor(4.91))
+
+print("Logaritma: ", math.log(100))
+print("Sinüs: ", math.sin(-1))
+print("Sinüs: ", math.sin(math.radians(90)))
+print("Cosinüs: ", math.cos(math.radians(90)))
+
+print("Rastgele tam sayı: ", randint(1,10)) # 1<=NUMBER<=10
+print("Rastgele ondalıklı sayı: ", uniform(0,1))
