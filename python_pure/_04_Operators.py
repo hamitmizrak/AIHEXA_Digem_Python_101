781ca3c56d5c2289095d9d547159d6728c4e74b0
HamitMizrak <hamitmizrak@gmail.com>
2026-04-01 14:28:59 +0300

    Operators * aritmetics * Logic

---
+++ python_pure/_04_Operators.py
@@ -0,0 +1,27 @@
+# Operators
+
+#########################################################
+#### Sayısal İşlemler
+# + - * / %
+number1=10
+number2=3
+print("Toplama: ", (number1+number2))
+print("Çıkarma: ", (number1-number2))
+print("Çarpma: ", (number1*number2))
+print("Bölme: ", (number1/number2))
+print("Bölme Kalan: ", (number1%number2))
+
+
+
+#########################################################
+#### Logic İşlemler
+print("Eşit mi ? ", (number1==number2))
+print("Eşit değil mi ? ", (number1!=number2))
+print("Büyük mü ", (number1>number2))
+print("Büyük Eşit mi ? ", (number1>=number2))
+print("Küçük mü ", (number1<number2))
+print("Küçük, Eşit mi ? ", (number1<=number2))
+
+# && VE
+# || VEYA
+# !  Değil
