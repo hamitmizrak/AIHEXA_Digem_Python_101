00b2026dce9b3a3cbf516fbe6017ade855716b9e
HamitMizrak <hamitmizrak@gmail.com>
2026-04-01 15:28:11 +0300

    conditional

---
+++ python_pure/_07_conditional.py
@@ -0,0 +1,28 @@
+
+##### Conditional
+
+# if else
+number=8
+if number > 0:
+    print("pozitif sayı")
+else:
+    print("negatif sayı")
+
+# ternary
+terna = "pozitif sayı" if number > 0 else "negatif sayı"
+print(terna)
+
+
+number=5
+if number==1:
+    print("sayı 1")
+elif number==2:
+    print("sayı 2")
+elif number==3:
+    print("sayı 3")
+elif number==4:
+    print("sayı 4")
+elif number==5:
+    print("sayı 5")
+else :
+    print("beş dışında bir sayı verildi")
