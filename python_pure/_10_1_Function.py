15943170bc71563c1b55a902669d6dcb13f9f358
HamitMizrak <hamitmizrak@gmail.com>
2026-04-06 14:47:02 +0300

    functional

--- python_pure/_10_1_Function.py
+++ python_pure/_10_1_Function.py
@@ -1,5 +1,6 @@
 # Function
 
+##################################################
 # 1- parametresiz returnsuz
 def hesap_topla1():
     """
@@ -13,7 +14,7 @@ def hesap_topla1():
 # Functional calling
 hesap_topla1()
 
-
+##################################################
 # 2- parametreli returnsuz
 def hesap_topla2(sayi1,sayi2):
     """
@@ -24,3 +25,55 @@ def hesap_topla2(sayi1,sayi2):
 
 # Functional calling
 hesap_topla2(10,20)
+print()
+
+
+##################################################
+# Default
+def default_toplam(sayi1,sayi2=90):
+    """
+    parametreli returnsuz toplam
+    """
+    sayi3=sayi1+sayi2
+    print(f"Toplam: {sayi3}")
+
+# Functional calling
+default_toplam(10)
+default_toplam(10,20)
+print()
+
+##################################################
+# 3- parametresiz returnlu
+def hesap_topla3():
+    """
+    parametresiz returnlu toplam
+    """
+    sayi1=100
+    sayi2=30
+    sayi3=sayi1+sayi2
+    return sayi3
+
+# Functional calling
+result=hesap_topla3()
+print(f"Toplam: {result}")
+print()
+
+
+
+##################################################
+# 4- parametreli,default returnlu
+def hesap_topla4(sayi1=20,sayi2=90):
+    """
+    parametresiz returnlu toplam
+    """
+    sayi3=sayi1+sayi2
+    return sayi3
+
+# Functional calling
+result2=hesap_topla4()
+print(f"Toplam: {result2}")
+
+result2=hesap_topla4(40,15)
+print(f"Toplam: {result2}")
+
+print()
