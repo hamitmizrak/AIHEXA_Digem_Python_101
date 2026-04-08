00decae28b834d673eac1533dd1b1bcf365965cc
HamitMizrak <hamitmizrak@gmail.com>
2026-04-08 13:29:28 +0300

    List, Set, Tupple

--- python_pure/_12_1_datatypes_all.py
+++ python_pure/_12_1_datatypes_all.py
@@ -9,10 +9,17 @@ print()
 
 
 ########################################################################
-############ SET ######################################################
+############ SET #######################################################
 # Type -> Set -->  Tekrarsız Veriler
 my_set1={1,2,9,3,"Malatya",True,14.53,"Malatya"}
 
 print(f"Set --> {type(my_set1)}")
 print(f"Set --> {my_set1}")
+print()
 
+########################################################################
+############ TUPPLE#####################################################
+# Type -> Tupple --> immutable(Değişmez) --> Tekrarlı veya Tekrarsız veri
+my_tupple1= (1,2,9,3,"Malatya",True,14.53,"Malatya")
+print(f"Tupple --> {type(my_tupple1)}")
+print(f"Tupple --> {my_tupple1}")
