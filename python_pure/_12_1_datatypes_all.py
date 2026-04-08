5fef0d20d18a0f7b173deba67c4e63078dd29c02
HamitMizrak <hamitmizrak@gmail.com>
2026-04-08 13:39:32 +0300

    List, Set, Tupple, Dictionary

--- python_pure/_12_1_datatypes_all.py
+++ python_pure/_12_1_datatypes_all.py
@@ -1,6 +1,7 @@
 ########################################################################
 ############ LIST ######################################################
 # Type -> List --> Tekrarlı veya Tekrarsız Veriler
+# List --> <class 'list'>
 my_list1=[1,2,9,3,"Malatya",True,14.53,"Malatya"]
 
 print(f"List --> {type(my_list1)}")
@@ -9,17 +10,34 @@ print()
 
 
 ########################################################################
+############ TUPPLE ####################################################
+# Type -> Tupple -->  Tekrarlı veya Tekrarsız veri ancak immutable(Değişmez)
+# Tupple --> <class 'tuple'>
+my_tupple1= (1,2,9,3,"Malatya",True,14.53,"Malatya")
+print(f"Tupple --> {type(my_tupple1)}")
+print(f"Tupple --> {my_tupple1}")
+
+
+########################################################################
 ############ SET #######################################################
 # Type -> Set -->  Tekrarsız Veriler
+# Set --> <class 'set'>
 my_set1={1,2,9,3,"Malatya",True,14.53,"Malatya"}
 
 print(f"Set --> {type(my_set1)}")
 print(f"Set --> {my_set1}")
 print()
 
+
 ########################################################################
-############ TUPPLE#####################################################
-# Type -> Tupple --> immutable(Değişmez) --> Tekrarlı veya Tekrarsız veri
-my_tupple1= (1,2,9,3,"Malatya",True,14.53,"Malatya")
-print(f"Tupple --> {type(my_tupple1)}")
-print(f"Tupple --> {my_tupple1}")
+############ DICTIONARY ################################################
+# Type -> Dictionary --> Key,Value
+# Dictionary <class 'dict'>
+person ={
+    "name":"Hamit",
+    "surname":"Mızrak",
+    "is_login":True,
+    "number":4423
+}
+print(f"Dictionary {type(person)}")
+print(f"Dictionary {person}")
