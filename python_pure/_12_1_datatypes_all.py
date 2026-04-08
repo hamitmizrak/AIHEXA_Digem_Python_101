5869994d5926ef22589e577f82ad7311c33452ee
HamitMizrak <hamitmizrak@gmail.com>
2026-04-08 13:19:12 +0300

    List, Set

---
+++ python_pure/_12_1_datatypes_all.py
@@ -0,0 +1,18 @@
+########################################################################
+############ LIST ######################################################
+# Type -> List --> Tekrarlı veya Tekrarsız Veriler
+my_list1=[1,2,9,3,"Malatya",True,14.53,"Malatya"]
+
+print(f"List --> {type(my_list1)}")
+print(f"List --> {my_list1}")
+print()
+
+
+########################################################################
+############ SET ######################################################
+# Type -> Set -->  Tekrarsız Veriler
+my_set1={1,2,9,3,"Malatya",True,14.53,"Malatya"}
+
+print(f"Set --> {type(my_set1)}")
+print(f"Set --> {my_set1}")
+
