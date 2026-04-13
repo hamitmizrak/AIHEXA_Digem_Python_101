0a8b0ca7f45ea9b610c51f8a132eeedb3f1b1ee8
HamitMizrak <hamitmizrak@gmail.com>
2026-04-13 15:26:29 +0300

    Class constructor Getter and setter

--- python_pure/_17_2_Class_Constructor.py
+++ python_pure/_17_2_Class_Constructor.py
@@ -12,7 +12,30 @@ class ArabaConstructor:
         print(f"Marka: {self.marka}  model:{self.model} yıl:{self.yil} renk:{self.yil}")
 
 
-    # SETTER AND SETTER
+    # SETTER AND GETTER
+    def set_marka(self,marka):
+        self.marka=marka
+
+    def get_marka(self):
+        return self.marka
 
 
+    def set_model(self,model):
+        self.model=model
 
+    def get_model(self):
+        return self.model
+
+
+    def set_yil(self,yil):
+        self.yil=yil
+
+    def get_yil(self):
+        return self.yil
+
+
+    def set_renk(self,renk):
+        self.renk=renk
+
+    def get_renk(self):
+        return self.renk
