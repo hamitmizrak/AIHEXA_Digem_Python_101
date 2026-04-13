44d13c370dc2be422b6e2fbc4b81914f5a93b93a
HamitMizrak <hamitmizrak@gmail.com>
2026-04-13 15:16:54 +0300

    Class constructor

--- python_pure/_17_2_Class_Constructor.py
+++ python_pure/_17_2_Class_Constructor.py
@@ -1,3 +1,4 @@
+# Class
 class ArabaConstructor:
     def __init__(self,marka="Bilinmiyor",model="bilinmiyor",yil=0,renk="bilinmiyor"):
         self.marka=marka
@@ -5,3 +6,13 @@ class ArabaConstructor:
         self.yil=yil
         self.renk=renk
         print("Araba için yeni bir sınıf oluşturdum")
+
+    # Function name
+    def bilgileri_goster(self):
+        print(f"Marka: {self.marka}  model:{self.model} yıl:{self.yil} renk:{self.yil}")
+
+
+    # SETTER AND SETTER
+
+
+
