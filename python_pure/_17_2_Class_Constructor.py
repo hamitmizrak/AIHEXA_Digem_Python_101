5ef0fbc4af128b1acf091b931065e1f9496f85f5
HamitMizrak <hamitmizrak@gmail.com>
2026-04-13 14:55:03 +0300

    Class constructor

---
+++ python_pure/_17_2_Class_Constructor.py
@@ -0,0 +1,7 @@
+class ArabaConstructor:
+    def __init__(self,marka="Bilinmiyor",model="bilinmiyor",yil=0,renk="bilinmiyor"):
+        self.marka=marka
+        self.model=model
+        self.yil=yil
+        self.renk=renk
+        print("Araba için yeni bir sınıf oluşturdum")
